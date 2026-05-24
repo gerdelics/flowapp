5cb25c9db5493134f90a8e5a2f622b24972dace9
Gerzson Erdeli <gerzson@example.com>
2026-05-24 08:41:48 +0200

    Add global Leaflet subdomain safety patch

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import 'leaflet/dist/leaflet.css'
 import App from './App.jsx'
+import { applyLeafletSafetyPatch } from './utils/leafletPatch'
+
+applyLeafletSafetyPatch()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
